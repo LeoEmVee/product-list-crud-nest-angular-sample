1dcfa8b0a2aa9f299c5cf99afa2078aa161d0bd9
LeoEmVee <santito79@hotmail.com>
2022-04-07 00:44:06 +0200

    Add Post(create) method to product.controller, install @nestjs/config, fix process.env bug, update app.module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT;
+console.log(port);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  await app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
 }
 bootstrap();

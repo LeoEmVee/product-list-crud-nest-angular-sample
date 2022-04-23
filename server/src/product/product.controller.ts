610fe35bc261606dbedb73a39560e84af8cb3a35
Santiago Mesa Villa <santito79@hotmail.com>
2022-04-23 18:42:32 +0200

    Add controllers for createProduct and findAllProducts. Update ProductSchema

--- server/src/product/product.controller.ts
+++ server/src/product/product.controller.ts
@@ -9,14 +9,37 @@ import {
   HttpStatus,
   Body,
 } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
+  constructor(private productService: ProductService) {}
+
   @Post('/create')
-  createProduct(@Res() res: any, @Body() createProductDTO: CreateProductDTO) {
-    console.log(createProductDTO);
+  async createProduct(
+    @Res() res: any,
+    @Body() createProductDTO: CreateProductDTO,
+  ) {
+    const newProduct = await this.productService.createProduct(
+      createProductDTO,
+    );
     return res.status(HttpStatus.OK).json({
-      message: 'Received',
+      message: 'Product successfully created',
+      'Product details': newProduct,
+    });
+  }
+
+  @Get('/')
+  async findAllProducts(@Res() res) {
+    const allProducts = await this.productService.findAllProducts();
+    return res.status(HttpStatus.OK).json({
+      message:
+        'Products successfully retrieved. ' +
+        `${
+          (allProducts.length && 'Here they are:') ||
+          'But currently there are none'
+        }`,
+      Products: allProducts,
     });
   }
 }
--- server/src/product/schemas/product.schema.ts
+++ server/src/product/schemas/product.schema.ts
@@ -1,12 +1,15 @@
 import { Schema } from 'mongoose';
 
-export const ProductSchema = new Schema({
-  name: String,
-  description: String,
-  imageURL: String,
-  price: Number,
-  createdAt: {
-    type: Date,
-    default: Date.now,
+export const ProductSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    imageURL: { type: String, required: true },
+    price: { type: Number, required: true },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-});
+  { versionKey: false },
+);

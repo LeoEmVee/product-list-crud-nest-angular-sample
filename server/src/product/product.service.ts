d1e0b70fd62a26c836a9c23ee7a25ec9004408f1
Santiago Mesa Villa <santito79@hotmail.com>
2022-04-23 17:28:57 +0200

    Add product interface and import it to Product Service. Add ProductService methods (create, findall, findone, delete, update)

--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product } from './interfaces/product.interface';
+import { CreateProductDTO } from './dto/product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel('Product') private readonly productModel: Model<Product>,
+  ) {}
+
+  async createProduct(createProductDTO: CreateProductDTO): Promise<Product> {
+    const newProduct = new this.productModel(createProductDTO);
+    return await newProduct.save();
+  }
+
+  async findAllProducts(): Promise<Product[]> {
+    const products = await this.productModel.find();
+    return products;
+  }
+
+  async findOneProduct(productID: string): Promise<Product> {
+    const product = await this.productModel.findById(productID);
+    return product;
+  }
+
+  async updateProduct(
+    productID: string,
+    createProductDTO: CreateProductDTO,
+  ): Promise<Product> {
+    const updatedProduct = await this.productModel.findByIdAndUpdate(
+      productID,
+      createProductDTO,
+      { new: true },
+    );
+    return updatedProduct;
+  }
+
+  async deleteProduct(productID: string): Promise<Product> {
+    const deletedProduct = await this.productModel.findByIdAndDelete(productID);
+
+    return deletedProduct;
+  }
+}

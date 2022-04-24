48a8503fee1460bcbd26a48da2396d4eba659b70
Santiago Mesa Villa <santito79@hotmail.com>
2022-04-24 02:18:29 +0200

    Define Product interface. Add methods to product service (create, findall, findone, update, delete), no functionality yet

--- client/src/app/services/product.service.ts
+++ client/src/app/services/product.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../interfaces/Product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  createProduct(product: Product) {}
+  findAllProducts() {}
+  findOneProduct(id: string) {}
+  updateProduct(id: string, product: Product) {}
+  deleteProduct(id: string) {}
 }
